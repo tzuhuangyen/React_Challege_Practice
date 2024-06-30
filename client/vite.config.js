84866ba39e5fc46b1a0a39dc442d4d04ac221d61
tzuhuangyen <912yan@gmail.com>
2024-06-30 14:27:31 +0100

    gh-pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React_Challege_Practice/',
+  base:
+    process.env.NODE_ENV === 'production' ? '/React_Challege_Practice/' : '',
+
   plugins: [react()],
   build: {
     rollupOptions: {
